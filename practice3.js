da035a98f337f02d9a8b42dd661fcef6f480f550
Jonathan Hatekah <102974087+underagee@users.noreply.github.com>
2023-02-18 01:43:42 +0000

    Primitive / Value Types

---
+++ practice3.js
@@ -0,0 +1,5 @@
+let name = "Joanthan"; // String literal
+let age = 12; // Number literal
+let isApproved = true; // Boolean literla
+let firstname = undefined;
+let colorSelection = null; // explicitly for clearing the vaalue of a variable
