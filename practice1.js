42df559d7022f158c3a68c7e3465466455101aad
Jonathan Hatekah <102974087+underagee@users.noreply.github.com>
2023-02-18 01:29:35 +0000

    Variable Declaration

---
+++ practice1.js
@@ -0,0 +1,13 @@
+let name = "Jonathan";
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a hyphen
+// Cannot contain a space or hyphen(-)
+// Are case-sensitive
+
+let firstname = "Jonathan";
+let lastname = "Hatekah";
+console.log(firstname);
+console.log(lastname);
