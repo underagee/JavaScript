78c24ff209af7d2be1788f61137b44ecd1007ffa
Jonathan Hatekah <102974087+underagee@users.noreply.github.com>
2023-03-19 16:52:20 +0000

    Objects

---
+++ practice5.js
@@ -0,0 +1,12 @@
+// Objects
+let person = {
+  name: "Jonathan",
+  age: 30,
+};
+// Dot Notation
+person.name = "Hatekah";
+
+//Bracket Notation
+person["name"] = "Mawuko";
+
+console.log(person.name);
